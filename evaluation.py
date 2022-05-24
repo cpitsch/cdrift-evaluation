ce302cc7583859ea3b665aa7ac797f784feeb689
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2022-05-24 23:54:19 +0200

    Implement changepoint assignment and average lag calculation Using LP with PULP library

--- evaluation.py
+++ evaluation.py
@@ -9,7 +9,8 @@ import pandas as pd
 from helpers import calcAvgDuration
 import matplotlib.pyplot as plt
 from deprecated import deprecated
-
+from pulp import LpProblem, LpMinimize, LpMaximize, LpVariable, LpBinary, lpSum
+import uuid
 # The calculation of the F1 score as described in "Change Point Detection and Dealing with Gradual and Multi-Order Dynamics in Process Mining" by Martjushev, Bose, Van Der Aalst
 def F1_Score(lag:int, detected:List[int], known: List[int], zero_division="warn", verbose:bool=False):
     """
@@ -104,7 +105,130 @@ def calcPrecisionRecall(lag:int, detected:List[int], known:List[int], zero_divis
         recall = zero_division
     return (precision, recall)
 
+def _assign_changepoints(detected_changepoints: List[int], actual_changepoints:List[int], lag_window:int=200):
+    """Assigns detected changepoints to actual changepoints.
+        With restrictions: 
+            - Detected point must be within lag_window of actual point. 
+            - Detected point can only be assigned to one actual point.
+            - Every actual point can have at most one detected point assigned. 
 
+    Args:
+        detected_changepoints (List[int]): List of locations of detected changepoints.
+        actual_changepoints (List[int]): List of locations of actual changepoints.
+        lag_window (int, optional): How close must a detected change point be to an actual changepoint to be a true positive. Defaults to 200.
+
+    Returns:
+        List[Tuple[int,int]]: List of tuples of (detected_changepoint, actual_changepoint) assignments
+    """
+
+    def buildProb_NoObjective(sense):
+        """
+            Builds the optimization problem, minus the Objective function. Makes multi-objective optimization simple
+        """
+        prob = LpProblem("Changepoint_Assignment", sense)
+
+        # Create a variable for each pair of detected and actual changepoints
+        vars = LpVariable.dicts("x", (detected_changepoints, actual_changepoints), 0, 1, LpBinary) # Assign detected changepoint dp to actual changepoint ap?
+        
+        # Flatten vars into dict of tuples of keys
+        x = {
+            (dc, ap): vars[dc][ap] for dc in detected_changepoints for ap in actual_changepoints
+        }
+
+        ####### Constraints #########
+        # As many assigments as possible
+        # prob += (
+        #     lpSum(x[dp, ap] for dp in detected_changepoints for ap in actual_changepoints) == tp,
+        #     "As_Many_As_Possible"
+        # )
+
+        # Only assign at most one changepoint to each actual changepoint
+        for ap in actual_changepoints:
+            prob += (
+                lpSum(x[dp, ap] for dp in detected_changepoints) <= 1,
+                f"Only_One_Changepoint_Per_Actual_Changepoint : {ap}"
+            )
+        # Each detected changepoint is assigned to at most one actual changepoint
+        for dp in detected_changepoints:
+            prob += (
+                lpSum(x[dp, ap] for ap in actual_changepoints) <= 1,
+                f"Only_One_Actual_Changepoint_Per_Detected_Changepoint : {dp}"
+            )
+        # Distance between chosen changepoints must be within lag window
+        for dp in detected_changepoints:
+            for ap in actual_changepoints:
+                prob += (
+                    x[dp, ap] * (dp - ap) <= lag_window,
+                    f"Distance_Within_Lag_Window : {dp}_{ap}"
+                )
+        return prob, x
+
+    ### Multi-Objective Optimization: First maximize number of assignments to find out the best True Positive number that can be achieved
+    # Find the largest number of change points:
+    prob1, prob1_vars = buildProb_NoObjective(LpMaximize)
+    prob1 += (
+        lpSum(
+            # Minimize the squared distance between assigned changepoints
+            prob1_vars[dp, ap]
+            for dp in detected_changepoints for ap in actual_changepoints
+        ),
+        "Maximize number of assignments"
+    )
+    prob1.solve()
+    # Calculate number of TP
+    num_tp = len([
+        (dp, ap)
+        for dp in detected_changepoints for ap in actual_changepoints
+        if prob1_vars[dp, ap].varValue == 1
+    ])
+
+
+    ### Multi-Objective Optimization: Now minimize the squared distance between assigned changepoints, using this maximal number of assignments
+    # Use this number as the number of assignments for second optimization
+    prob2, prob2_vars = buildProb_NoObjective(LpMinimize)
+    prob2 += (
+        lpSum(
+            # Minimize the squared distance between assigned changepoints
+            prob2_vars[dp, ap] * pow(dp - ap,2)
+            for dp in detected_changepoints for ap in actual_changepoints
+        ),
+        "Squared_Distances"
+    )
+
+    # Number of assignments is the number of true positives we found in the first optimization
+    prob2 += (
+        lpSum(
+            prob2_vars[dp, ap]
+            for dp in detected_changepoints for ap in actual_changepoints
+        ) == num_tp,
+        "Maximize Number of Assignments"
+    )
+    prob2.solve()
+    return [
+        (dp, ap)
+        for dp in detected_changepoints for ap in actual_changepoints
+        if prob2_vars[dp, ap].varValue == 1
+    ]
+
+def get_avg_lag(detected_changepoints:List[int], actual_changepoints:List[int], lag_window:int=200):
+    """Calculates the average lag between detected and actual changepoints (false positives do not affect this metric!)
+
+    Args:
+        detected_changepoints (List[int]): Locations of detected changepoints
+        actual_changepoints (List[int]): Locations of actual (known) changepoints
+        lag_window (int, optional): How close must a detected change point be to an actual changepoint to be a true positive. Defaults to 200.
+
+    Returns:
+        float: the average distance between detected changepoints and the actual changepoint they get assigned to
+    """
+    assignments = _assign_changepoints(detected_changepoints, actual_changepoints, lag_window=lag_window)
+    avg_lag = 0
+    for (dc,ap) in assignments:
+        avg_lag += abs(dc-ap)
+    try:
+        return avg_lag/len(assignments)
+    except ZeroDivisionError:
+        return np.nan;
 
 
 def getROCData(lag:int, df:pd.DataFrame, undefined_equals=0):
