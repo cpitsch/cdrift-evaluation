15392e446f91f2b4955ecef1f070843da1b81c9b
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2022-08-31 19:50:59 +0200

    Commit safe_update_bar in helpers. Forgot to commit previously

--- cdrift/utils/helpers.py
+++ cdrift/utils/helpers.py
@@ -109,6 +109,10 @@ def makeProgressBar(num_iters:int=None, message:str="", position:int=None):
 
     return  tqdm(total=num_iters, desc=f"{message} :: ", position=position, leave=True)
 
+def safe_update_bar(progress_bar, amount:int)->None:
+    if progress_bar is not None:
+        progress_bar.update(amount)
+
 def _getNumActivities(log:EventLog, activityName_key:str=xes.DEFAULT_NAME_KEY)->int:
     """A helper function to calculate the number of activities in an event log.
 
