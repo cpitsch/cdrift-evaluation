29eb21cb116f7deb9398974a9fb7da064f9a8d98
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2022-08-31 20:07:55 +0200

    Update Martjushev __init__.py

--- cdrift/approaches/martjushev/__init__.py
+++ cdrift/approaches/martjushev/__init__.py
@@ -1,1 +1,9 @@
-from cdrift.approaches.martjushev.martjushev import *
+from cdrift.approaches.martjushev.martjushev import \
+    detectChange_JMeasure_KS,\
+    detectChange_JMeasure_MU,\
+    detectChange_WindowCount_KS,\
+    detectChange_WindowCount_MU, \
+    detectChange_ADWIN_JMeasure_KS,\
+    detectChange_ADWIN_JMeasure_MU,\
+    detectChange_ADWIN_WindowCount_KS,\
+    detectChange_ADWIN_WindowCount_MU
